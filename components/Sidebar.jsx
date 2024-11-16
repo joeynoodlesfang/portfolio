b53a2180b9b3d8d15b3303710c3878810a0a51bb
jf <linzhoufang@gmail.com>
2024-11-16 03:05:53 -0800

    wip3

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React from "react"
-import GithubIcon from "/images/GithubIcon.svg"
-import LinkedInIcon from "/images/LinkedInIcon.svg"
+import GithubIcon from "../images/GithubIcon.svg"
+import LinkedInIcon from "../images/LinkedInIcon.svg"
 
 export default function Sidebar() {
     return (
