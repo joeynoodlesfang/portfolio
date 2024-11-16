cce385f97c717f5e7c31e8cf45567f59824e373a
jf <linzhoufang@gmail.com>
2024-11-16 02:47:05 -0800

    wip

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -3,6 +3,7 @@ import React from "react"
 export default function Sidebar() {
     return (
         <div className="sidebar--container">
+            <p>test</p>
             <div className="sidebar--footer">
                 <img src="/images/GitHubIcon.svg" />
                 <img src="/images/LinkedInIcon.svg" />
